c08337b75e45e7335ab3769b9f779bcd483007ad
Edgar <jorge260590@hotmail.com>
2024-02-06 13:30:33 -0500

    style(genelar): Add a checkout to the project and some style stuff

--- src/data/db.js
+++ src/data/db.js
@@ -3,100 +3,105 @@ const Projects = [
         "id": "1",
         "title": "Expanding Cards",
         "description": "Animated Slide Expanding Image",
-        "link":"https://65afd7711706ab1e832649d0--stupendous-vacherin-a8d787.netlify.app/",
+        "link": "https://65afd7711706ab1e832649d0--stupendous-vacherin-a8d787.netlify.app/",
         "mock": "/assets/img/mocks/expandingCards.png"
     },
     {
-        "id":"2",
+        "id": "2",
         "title": "Progress Steps",
         "description": "Animated bar progress steps",
-        "link":"https://65afd7d824831a22cfde6563--grand-zabaione-4d783d.netlify.app/",
-        "mock":"/assets/img/mocks/progressSteps.png"
+        "link": "https://65afd7d824831a22cfde6563--grand-zabaione-4d783d.netlify.app/",
+        "mock": "/assets/img/mocks/progressSteps.png"
     },
     {
         "id": "3",
-        "title":"Rotating Page",
+        "title": "Rotating Page",
         "description": "Animated rotating menu navigation",
         "link": "https://65afd97786bab1245360ab3f--chic-sunflower-649483.netlify.app/",
-        "mock":"/assets/img/mocks/rotatingPage.png"
+        "mock": "/assets/img/mocks/rotatingPage.png"
     },
     {
         "id": "4",
-        "title":"Expanding Search",
+        "title": "Expanding Search",
         "description": "Animated hiding search widget",
         "link": "https://65afe376f83a69007a6a778c--elegant-marigold-23b15e.netlify.app/",
-        "mock":"/assets/img/mocks/expandingSearch.png"
+        "mock": "/assets/img/mocks/expandingSearch.png"
     },
     {
         "id": "5",
-        "title":"Blurry Loading",
+        "title": "Blurry Loading",
         "description": "Blur loading transition",
         "link": "https://65afea54a52813040bd0bd4e--tangerine-boba-0e1c46.netlify.app/",
-        "mock":"/assets/img/mocks/blurryLoading.png"
+        "mock": "/assets/img/mocks/blurryLoading.png"
     },
     {
         "id": "6",
-        "title":"Scroll Animation",
+        "title": "Scroll Animation",
         "description": "Animated scroll content",
         "link": "https://65aff67f9b35cf10bd02613c--effortless-concha-b8f4b5.netlify.app/",
-        "mock":"/assets/img/mocks/scrollAnimation.png"
+        "mock": "/assets/img/mocks/scrollAnimation.png"
     },
     {
         "id": "7",
-        "title":"Split Landing Page",
+        "title": "Split Landing Page",
         "description": "Hovering animated split landing page",
         "link": "https://65b3f7b9d861a944a5dcf612--fancy-monstera-8bf040.netlify.app/",
-        "mock":"/assets/img/mocks/splitLandingPage.png"
+        "mock": "/assets/img/mocks/splitLandingPage.png"
     },
     {
         "id": "8",
-        "title":"Form Input Wave",
+        "title": "Form Input Wave",
         "description": "Animated Input Label",
         "link": "https://65b804db45a76b44e811d0af--subtle-frangipane-215f1b.netlify.app/",
-        "mock":"/assets/img/mocks/formInputWave.png"
+        "mock": "/assets/img/mocks/formInputWave.png"
     },
     {
         "id": "9",
-        "title":"Sound Board",
+        "title": "Sound Board",
         "description": "Simple Board interface to hear some funny sounds",
         "link": "https://65b84ac5b0e7d073134d0fc2--serene-salmiakki-ed1884.netlify.app/",
-        "mock":"/assets/img/mocks/soundBoard.png"
+        "mock": "/assets/img/mocks/soundBoard.png"
     },
     {
         "id": "10",
-        "title":"Bad Jokes",
+        "title": "Bad Jokes",
         "description": "A simple Bad Jokes Api",
         "link": "https://65b85380303f707b96cb8cab--glowing-tartufo-69fc85.netlify.app/",
-        "mock":"/assets/img/mocks/badJokes.png"
+        "mock": "/assets/img/mocks/badJokes.png"
     },
     {
         "id": "11",
-        "title":"KeyCode",
+        "title": "KeyCode",
         "description": "You can used to see the key code",
         "link": "https://65b9bd85351bb07eae75bac6--verdant-salamander-42ada5.netlify.app/",
-        "mock":"/assets/img/mocks/keyCode.png"
+        "mock": "/assets/img/mocks/keyCode.png"
     },
     {
         "id": "12",
-        "title":"FAQ",
+        "title": "FAQ",
         "description": "Frequently Asked Questions Example",
         "link": "https://65bacddd70790333e67d9456--stirring-buttercream-e4545b.netlify.app/",
-        "mock":"/assets/img/mocks/FAQ.png"
+        "mock": "/assets/img/mocks/FAQ.png"
     },
     {
         "id": "13",
-        "title":"Random Choice Picker",
+        "title": "Random Choice Picker",
         "description": "A simple game to choose a random choice picker",
         "link": "https://65bc2b5409fcd008a9bb403d--resplendent-liger-ce9f26.netlify.app/",
-        "mock":"/assets/img/mocks/RCP.png"
+        "mock": "/assets/img/mocks/RCP.png"
     },
+
+
+
+
+
     {
-        "id": "14",
-        "title":"Animated Menu Navigation",
-        "description": "Simple Animation Nav",
-        "link": "",
-        "mock":"/assets/img/mocks/animatedMenu.png"
+        "id": "00",
+        "title": "Coming Soon",
+        "description": "",
+        "link": "#",
+        "mock": "/assets/img/soon.png"
     }
 
-] 
+]
 export default Projects;
